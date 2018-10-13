c9b7d24facfac2b18a19fa7777203d8c0e3f6e06
rafaelvelazquezmartin <rafavel97@hotmail.com>
2018-10-13 19:32:03 +0100

    Started on server

--- first-try-Friday/app.js
+++ first-try-Friday/app.js
@@ -1,35 +1,45 @@
+var socket = io.connect("/");
+//Now we can listen for that event
+socket.on("onconnected", function(data) {
+  //Note that the data is the object we sent from the server, as is. So we can assume its id exists.
+  console.log(
+    "Connected successfully to the socket.io server. My server side ID is " +
+      data.id
+  );
+});
+
 var num_of_objects = 2;
 var instructions = [];
-for(i = 0; i < num_of_objects; i++){
-	instructions[i] = [];
+for (i = 0; i < num_of_objects; i++) {
+  instructions[i] = [];
 }
 
 function allowDrop(ev) {
-    ev.preventDefault();
-	var pos = ev.target.id.split(",");
-	pos[1] = Math.min(pos[1], instructions[pos[0]].length);
-	var str = pos[0] + ',' + pos[1];
-	document.getElementById(str).parentElement.style.border = "2px solid yellow";
+  ev.preventDefault();
+  var pos = ev.target.id.split(",");
+  pos[1] = Math.min(pos[1], instructions[pos[0]].length);
+  var str = pos[0] + "," + pos[1];
+  document.getElementById(str).parentElement.style.border = "2px solid yellow";
 }
 
-function dragLeave(ev){
-	var pos = ev.target.id.split(",");
-	pos[1] = Math.min(pos[1], instructions[pos[0]].length);
-	var str = pos[0] + ',' + pos[1];
-	document.getElementById(str).parentElement.style.border = "2px solid black";
+function dragLeave(ev) {
+  var pos = ev.target.id.split(",");
+  pos[1] = Math.min(pos[1], instructions[pos[0]].length);
+  var str = pos[0] + "," + pos[1];
+  document.getElementById(str).parentElement.style.border = "2px solid black";
 }
 
 function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+  ev.dataTransfer.setData("text", ev.target.id);
 }
 
 function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-	var pos = ev.target.id.split(",");
-	pos[1] = Math.min(pos[1], instructions[pos[0]].length);
-	var str = pos[0] + ',' + pos[1];
-	document.getElementById(str).innerHTML = data;
-	instructions[pos[0]][pos[1]]= data;
-	document.getElementById(str).parentElement.style.border = "2px solid black";
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  var pos = ev.target.id.split(",");
+  pos[1] = Math.min(pos[1], instructions[pos[0]].length);
+  var str = pos[0] + "," + pos[1];
+  document.getElementById(str).innerHTML = data;
+  instructions[pos[0]][pos[1]] = data;
+  document.getElementById(str).parentElement.style.border = "2px solid black";
 }
---
+++ first-try-Friday/simple-server.js
@@ -0,0 +1,20 @@
+// Including libraries
+var express = require("express");
+var app = express();
+var server = app.listen(3000);
+var io = require("socket.io")(server);
+
+// Routing
+app.use(express.static("public"));
+
+// Listen for incoming connections from clients
+io.sockets.on("connection", function(socket) {
+  console.log("SOmeone connected");
+
+  // Start listening for mouse move events
+  socket.on("mousemove", function(data) {
+    // This line sends the event (broadcasts it)
+    // to everyone except the originating client.
+    socket.broadcast.emit("moving", data);
+  });
+});
